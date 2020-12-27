bedece5c5c8d5d88f9a7f1552e65da4369b776bb
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-26 20:22:35 -0800

    the component tableBody is created so it is reusable

--- src/components/moviesTables.jsx
+++ src/components/moviesTables.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import Like from './like';
 import TableHeader from './tableHeader';
+import TableBody from './tableBody';
 
 class MoviesTable extends Component {
+
+  
 
   columns=[
    { path: 'title', label:'Title' },
    { path: 'genre.name', label:'Genre' },
    { path: 'numberInStock', label:'Stock' },
    { path: 'dailyRentalRate', label:'Rate' },
-   { key:'like' },
-   { key:'delete' }
+   { 
+     key:'like', 
+     content: movie=> <Like liked={movie.liked} onClick={()=>this.props.onLike(movie)}/>  
+    },
+   { key:'delete',
+     content: movie=> (<button
+     onClick={()=>this.props.onDelete(movie)}
+     className="btn btn-danger btn-sm"
+   >
+   delete
+   </button>
+     )
+   }
   ];
   
   render() { 
-    const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
+    const { movies, sortColumn, onSort } = this.props;
 
     return (
       <table className="table">
@@ -23,26 +37,8 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort ={onSort}
         />
-        <tbody>
-          {movies.map(movie=> 
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like liked={movie.liked} onClick={()=>onLike(movie)}/>
-            </td>
-            <td>
-              <button
-                onClick={()=>onDelete(movie)}
-                className="btn btn-danger btn-sm"
-              >
-              delete
-              </button>
-            </td>
-          </tr>)}
-        </tbody>
+        <TableBody data={movies} columns={this.columns}/>
+        
       </table>
     );
   }
