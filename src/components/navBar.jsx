56a1ba9a4e0e030b25c2b09237dee4b98196e73e
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-27 21:20:57 -0800

    navBar is imported

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom'
 
-class NavBar extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#">Vidly</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-       </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+const NavBar = () => {
+  return ( 
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+      <Link className="navbar-brand" to="/">
+        Vidly
+      </Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+         <NavLink className="nav-link" to="/movies">Movies</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li className="nav-item">
+         <NavLink className="nav-link" to="/customers">Customers</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li className="nav-item">
+         <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
         </li>
       </ul>
-      <span class="navbar-text">
-        Navbar text with an inline element
-      </span>
     </div>
   </div>
 </nav>
-     );
-  }
+   );
 }
  
 export default NavBar;
