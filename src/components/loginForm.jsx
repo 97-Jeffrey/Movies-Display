930f796d56b359d5c0a07a25a80308099205ace3
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-28 16:26:03 -0800

    the input field are modified so that the it is binded to state

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 
 class LoginFrom extends Component {
 
+  state={
+    account:{ username:'', password:'' }
+  };
+
   handleSubmit= e =>{
     e.preventDefault();
 
     console.log('submitted ')
   } 
+
+  handleChange= ({ currentTarget:input}) =>{
+    const account={...this.state.account};
+    account[input.name]=input.value;
+    this.setState({ account });
+  }
   
   render() { 
+
+    const { account } = this.state;
     return (
       <div>
        <h1>Login</h1>
        <form onSubmit={this.handleSubmit}>
          <div className="form-group">
            <label htmlFor="username">Username</label>
-           <input id='username' type="text" className="form-control"/>
+           <input 
+            value={account.username}
+            onChange={this.handleChange}
+            ref={this.username}
+            name='username'
+            id='username' 
+            type="text" 
+            className="form-control"/>
          </div>
          <div className="form-group">
            <label htmlFor="password">Password</label>
-           <input id='password'type="text" className="form-control"/>
+           <input
+            value={account.password}
+            onChange={this.handleChange}
+            name='password'
+            id='password'
+            type="text" 
+            className="form-control"/>
          </div>
          <button className="btn btn-primary">Login</button>
        </form>
