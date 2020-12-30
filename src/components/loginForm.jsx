66f2cbd7742a9c7994c50d05781f9f8eb3ad42c3
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-29 19:59:34 -0800

    the submit button is disabled when there is error messaged output from validate method

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -81,7 +81,10 @@ class LoginFrom extends Component {
           error={errors.password}
         />
          
-         <button className="btn btn-primary">Login</button>
+         <button 
+         disabled={this.validate()}
+         className="btn btn-primary"
+         >Login</button>
        </form>
       </div>
     );
