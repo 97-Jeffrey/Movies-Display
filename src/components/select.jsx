6640927bab7eb5eb38ec4c96939271ed9dca11bf
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-30 01:12:45 -0800

    space changed

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -12,6 +12,7 @@ const Select = ({ name, label, options, error, ...rest }) => {
           </option>
         ))}
       </select>
+      
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
